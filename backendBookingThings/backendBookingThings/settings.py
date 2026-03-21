75e17a0b2dad67c776024811623c11c832a76fec
Juan Esteban Calle <softwaretools731@gmail.com>
2026-03-21 14:04:55 -0500

    ADD: swagger

--- backendBookingThings/backendBookingThings/settings.py
+++ backendBookingThings/backendBookingThings/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_spectacular',
     'resource',
     'users'
 ]
@@ -49,6 +50,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
 SIMPLE_JWT = {
--- backendBookingThings/backendBookingThings/urls.py
+++ backendBookingThings/backendBookingThings/urls.py
@@ -8,6 +8,9 @@ from django.db import router
 from django.urls import path
 from users.views import RegisterView, LoginView
 
+# DOCS
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
 
 # JWT
 # from rest_framework_simplejwt.views import (
@@ -24,6 +27,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
 ]
